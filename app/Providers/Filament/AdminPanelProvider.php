54e2dc1d55732e542bc1c609e8c76de9087e0f17
Aleksandre Gugesashvili <rachvela@Aleksandres-MacBook-Pro.local>
2025-06-19 14:09:49 +0400

    remove password reset

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,7 +36,6 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->profile()
-            ->passwordReset()
             ->font('Monospace')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
